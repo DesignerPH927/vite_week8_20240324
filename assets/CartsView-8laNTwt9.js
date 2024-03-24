ba42347124b0960cbe13dace8bade1e603b5e1c5
DesignerPH927 <designer.ph927@gmail.com>
2024-03-24 23:47:20 +0800

    adding dist

---
+++ assets/CartsView-8laNTwt9.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as c,c as a,a as e,d as n}from"./index-6BvTerDF.js";const r={},_={class:"cart"},d=e("div",{class:"cartHeader"},null,-1),i={class:"container"},l={class:"py-7"};function p(f,m){const s=t("RouterView");return c(),a("div",_,[d,e("div",i,[e("div",l,[n(s)])])])}const h=o(r,[["render",p]]);export{h as default};
--- assets/CartsView-fsP1DdVb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as c,c as a,a as e,d as n}from"./index-IH5Zc-RJ.js";const r={},_={class:"cart"},d=e("div",{class:"cartHeader"},null,-1),i={class:"container"},l={class:"py-7"};function p(f,m){const s=t("RouterView");return c(),a("div",_,[d,e("div",i,[e("div",l,[n(s)])])])}const h=o(r,[["render",p]]);export{h as default};
